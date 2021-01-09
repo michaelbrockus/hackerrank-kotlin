3bb28bca12fd3b75ebcbb7bf5f1402f2772edb91
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-09 00:06:18 -0800

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,56 @@
 // gmail: <michaelbrockus@gmail.com>
 // github: https://github.com/michaelbrockus
 //
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+//
+// Complete the compareTriplets function below.
+//
+fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    var countAlice: Int = 0;
+    var countBob: Int = 0;
+
+    
+    for (iter in 0 until a.size)
+    {
+        if (a[iter] > b[iter])
+        {
+            countAlice++
+        }
+        else if (a[iter] < b[iter])
+        {
+            countBob++
+        }
+    }
+    return arrayOf(countAlice, countBob)
+}
 
 //
 // function main begins all program execution
 //
-fun main() = println("We are ready for HackerRank")
+fun main(args: Array<String>) {
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = compareTriplets(a, b)
+
+    println(result.joinToString(" "))
+}
