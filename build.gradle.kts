73fb8d020849f0fb4d96d28bf37107a4a099a0cd
Michael <michaelbrockus@gmail.com>
2021-01-08 20:16:30 -0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "blog.home.michaelbrockus"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+//
+// file: main.kt
+// author: Michael Brockus
+// gmail: <michaelbrockus@gmail.com>
+// github: https://github.com/michaelbrockus
+//
+
+//
+// function main begins all program execution
+//
+fun main() = println("We are ready for HackerRank")
