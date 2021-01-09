d31583d7db96976b7d7705c3275589bf7065cb15
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-08 22:02:26 -0800

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,44 @@
 // gmail: <michaelbrockus@gmail.com>
 // github: https://github.com/michaelbrockus
 //
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+//
+// Complete the aVeryBigSum function below.
+//
+fun aVeryBigSum(ar: Array<Long>): Long {
+     var summary: Long = 0
+     for (len in ar) {
+        summary += len
+     } // end for
+     return summary
+} // end of function aVeryBigSum
 
 //
 // function main begins all program execution
 //
-fun main() = println("We are ready for HackerRank")
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val arCount = scan.nextLine().trim().toInt()
+    val ar = scan.nextLine().split(" ").map{ it.trim().toLong() }.toTypedArray()
+    val result = aVeryBigSum(ar)
+
+    println(result)
+} // end of function main
