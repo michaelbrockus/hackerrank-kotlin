03f8ddc5bb88012243efd3d38402f450a6ef9576
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-11 22:19:23 -0800

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,51 @@
 // gmail: <michaelbrockus@gmail.com>
 // github: https://github.com/michaelbrockus
 //
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+//
+// Complete the staircase function below.
+//
+fun staircase(n: Int): Unit {
+    val staircaseArr = Array<Array<Char>>(n) { Array<Char>(n) {' '} }
+    for(iter in 0..n - 1) {
+        var step = 0
+        for (subIter in n-1 downTo 0) {
+            if (step < iter + 1) {
+                staircaseArr[iter][subIter] = '#'
+                step++
+            } // end if
+
+        } // end for
+        println((staircaseArr[iter]).joinToString(""))
+    } // end for
+} // end of function staircase
 
 //
 // function main begins all program execution
 //
-fun main() = println("We are ready for HackerRank")
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    staircase(n)
+} // end of function main
